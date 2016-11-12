670792cfc552787fdbee51aa55f14990b39b7ab8
scriptonist <aravind.techbridge@gmail.com>
2016-11-12 12:07:31 +0530

    Bussiness section complete

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ function(session,results){
 
         }
         else if(results.response.entity == "Bussiness"){
-            //Fetch Bussiness News
+            session.beginDialog("/Bussiness");
                 
         }
         else if(results.response.entity == "Entertaintment"){
@@ -208,6 +208,111 @@ bot.dialog('/SportsNews',[
             if(results.response.entity == "Others")
             {
                 api.getArticles("en","","","sports_other-sports",function(data){
+                    api.getArticleDetails("en",data["articleSummary"][0]['articleID'],function(data2){
+                         var msg = new builder.Message(session)
+                    .textFormat(builder.TextFormat.xml)
+                    .attachments([
+                        new builder.HeroCard(session)
+                            .title(data2['title'])
+                            .subtitle(data2['authorname'])
+                            .text(data2["imageDescription"])
+                            .images([
+                                builder.CardImage.create(session, data2['thumbnail'])
+                            ])
+                            .tap(builder.CardAction.openUrl(session, data2['articleURL']))
+                    ]);
+
+                         session.send(msg);
+                    });
+
+                   
+                });
+            }
+        }
+        session.endDialog();
+    }
+]);
+
+
+bot.dialog('/Bussiness',[
+    function(session){
+    builder.Prompts.choice(session,"Sections","General|Gadgets|Autos|Companies");
+    
+    },
+    function(session,results){
+        if(results.response){
+            if(results.response.entity == "General")
+            {
+                api.getArticles("en","","","business_news",function(data){
+                    api.getArticleDetails("en",data["articleSummary"][0]['articleID'],function(data2){
+                         var msg = new builder.Message(session)
+                    .textFormat(builder.TextFormat.xml)
+                    .attachments([
+                        new builder.HeroCard(session)
+                            .title(data2['title'])
+                            .subtitle(data2['authorname'])
+                            .text(data2["imageDescription"])
+                            .images([
+                                builder.CardImage.create(session, data2['thumbnail'])
+                            ])
+                            .tap(builder.CardAction.openUrl(session, data2['articleURL']))
+                    ]);
+
+                         session.send(msg);
+                    });
+
+                   
+                });
+            }
+            if(results.response.entity == "Gadgets")
+            {
+                api.getArticles("en","","","business_gadgets",function(data){
+                    api.getArticleDetails("en",data["articleSummary"][0]['articleID'],function(data2){
+                         var msg = new builder.Message(session)
+                    .textFormat(builder.TextFormat.xml)
+                    .attachments([
+                        new builder.HeroCard(session)
+                            .title(data2['title'])
+                            .subtitle(data2['authorname'])
+                            .text(data2["imageDescription"])
+                            .images([
+                                builder.CardImage.create(session, data2['thumbnail'])
+                            ])
+                            .tap(builder.CardAction.openUrl(session, data2['articleURL']))
+                    ]);
+
+                         session.send(msg);
+                    });
+
+                   
+                });
+            }
+            if(results.response.entity == "Autos")
+            {
+                api.getArticles("en","","","business_autos",function(data){
+                    api.getArticleDetails("en",data["articleSummary"][0]['articleID'],function(data2){
+                         var msg = new builder.Message(session)
+                    .textFormat(builder.TextFormat.xml)
+                    .attachments([
+                        new builder.HeroCard(session)
+                            .title(data2['title'])
+                            .subtitle(data2['authorname'])
+                            .text(data2["imageDescription"])
+                            .images([
+                                builder.CardImage.create(session, data2['thumbnail'])
+                            ])
+                            .tap(builder.CardAction.openUrl(session, data2['articleURL']))
+                    ]);
+
+                         session.send(msg);
+                    });
+
+                   
+                });
+            }
+            if(results.response.entity == "Companies")
+            {
+                api.getArticles("en","","","business_companies",function(data){
                     api.getArticleDetails("en",data["articleSummary"][0]['articleID'],function(data2){
                          var msg = new builder.Message(session)
                     .textFormat(builder.TextFormat.xml)
